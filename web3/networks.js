58f02911e39b79ba7a7b7049596858d3f895c1db
okx-wallet-H <hongchen000555@gmail.com>
2026-03-17 02:39:12 +0700

    创建 networks.js

---
+++ web3/networks.js
@@ -0,0 +1,23 @@
+export const networks = {
+
+bnb:{
+
+name:"BNB Chain",
+
+rpc:"https://bsc-dataseed.binance.org",
+
+chainId:56
+
+},
+
+xlayer:{
+
+name:"X Layer",
+
+rpc:"https://rpc.xlayer.tech",
+
+chainId:196
+
+}
+
+}
